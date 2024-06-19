57ad6140505b09a3abbbc47584ec7870d9473339
RokiroCMD <rokiro11000@gmail.com>
2024-06-19 11:06:06 -0800

    Initial commit

---
+++ src/main/java/net/rokiro/rokiromod/RokirosModClient.java
@@ -0,0 +1,11 @@
+package net.rokiro.rokiromod;
+
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RokirosModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
